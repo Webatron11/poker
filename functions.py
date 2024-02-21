62953826fafdf333ced1c5f2ee1e46881a4cbf6d
Oscar <oscarwebb162@protonmail.com>
2024-02-22 09:24:59 +1100

    Uhh comments... yuck

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ from data import *
 
 
 def chipstobalance(chips: str):
+    ### Uses regex to seperate inputed string into W, R, B, G chip counts and then creating a total balance
     regex = r'\d+\s*'
 
     matches = re.findall(regex, chips)
@@ -11,6 +12,8 @@ def chipstobalance(chips: str):
     for i in matches:
         formated.append(int(i.strip()))
 
+    # I can't actually rememeber why this is here.
+
     try:
         return (formated[0] * 10) + (formated[1] * 25) + (formated[2] * 100) + (formated[3] * 500)
     except IndexError:
@@ -18,8 +21,10 @@ def chipstobalance(chips: str):
 
 
 def balance(sessions, player: Player):
+    ### Appends sessions chip total for a player onto their balanceovertime variable. I can't remeber by the match case statement was needed, but it is.
     name = player.name
     for i in sessions:
+        # Chooses which player.balance to change depending on the inputed player variable, Also checks if the inputed chip string is empty.
         match name:
             case "Aidan":
                 if i.aidan != '':
@@ -61,18 +66,23 @@ def balance(sessions, player: Player):
 
 
 def buyin(sessions, player):
+    ### Goes through all the sessions and adds up a player's buyins by checking if their name appears in the buyin column of the session.
     for i in sessions:
         if player.name in i.buyins:
             player.buyins += 1
 
 
 def revbuyin(sessions, player):
+    ### Goes through all the sessions and adds up a player's revbuyins by checking if their name appears in the revbuyin column of the session.
     for i in sessions:
         if player.name in i.revbuyins:
             player.revbuyins += 1
 
 
 def profit(sessions, player):
+    ### It SHOULD take the player balance overtime, apply any buyins or revbuyins then append it to the profit list in order to create a profit over time
+    ### thing, but it doesn't work yet.
+
     for i in range(len(player.balanceovertime)):
         i -= 1
         if player.name in sessions[i].revbuyins:
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 from functions import *
 import matplotlib.pyplot as plt
 
+# Player initialisation. Adds a -600 profit from the get go because of the initial buyin. The 0.3 for buyins is for the initial 600 buyin before the number was changed
+
 aidan = Player("Aidan", 0, 0.3, 0, [0], [-600])
 ben = Player("Ben", 0, 0.3, 0, [0], [-600])
 cooper = Player("Cooper", 0, 0.3, 0, [0], [-600])
@@ -10,42 +12,52 @@ mitchell = Player("Mitchell", 0, 0.3, 0, [0], [-600])
 oscar = Player("Oscar", 0, 0.3, 0, [0], [-600])
 xavier = Player("Xavier", 0, 0.3, 0, [0], [-600])
 
+# Array of players in order to index through all players.
+
 players = [aidan, ben, cooper, hunter, mitchell, oscar, xavier]
 
 sessions = []
 
+# Reads csv file and parses the information into an array of the Session type.
+
 csv_read = ''
-with open('Poker.csv') as f:
+with open('Poker1.csv') as f:
     csv_read = csv.reader(f, delimiter=',')
 
     next(csv_read)
 
-    i = 1
+    i = 1 # This is for the session number.
     for row in csv_read:
         session = Session(i, row[1].split(';'), row[2].split(';'), row[3].split(';'), row[4], row[5], row[6], row[7], row[8], row[10], row[11])
         sessions.append(session)
         i += 1
 
+# Makes an array of integers so we can graph stuff based on session number.
+
 sessionnumbers = [0]
 for i in range(len(sessions)):
     sessionnumbers.append(i+1)
+
+# Does all the math stuff.
 
 for i in players:
     balance(sessions, i)
     buyin(sessions, i)
     revbuyin(sessions, i)
-    profit(sessions, i)
-    # i.balance = i.balance - (i.buyins * 2000) + (i.revbuyins * 2000)
+    # profit(sessions, i) # Non-functional function to be added later
+    i.balance = i.balance - (i.buyins * 2000) + (i.revbuyins * 2000) # This just creates a final balance based off buyins and revbuyins
     # TODO make final balance correct......
-    print(i.balance, i.balanceovertime, i.profit, i.buyins, i.revbuyins, i.name)
-    #
-    # plt.plot(sessionnumbers, i.balanceovertime, marker='x')
+    print(i.balance, i.balanceovertime, i.profit, i.buyins, i.revbuyins, i.name) # Bug checking for now, going to output data in a prettier manner later
+
+    # Plotting balanceovertime against session number. Should be self-explanatory
+
+    plt.plot(sessionnumbers, i.balanceovertime, marker='x')
     # TODO make it plot negative numbers, change scale of x, interactive????
-    # plt.ylim(min(i.balanceovertime), max(i.balanceovertime))
-    #
-    # plt.xlabel("Session Number")
-    # plt.ylabel("Balance")
-    # plt.title(i.name)
-    # plt.grid(True)
-    #
-    # plt.show()
+    plt.ylim(min(i.balanceovertime), max(i.balanceovertime))
+
+    plt.xlabel("Session Number")
+    plt.ylabel("Balance")
+    plt.title(i.name)
+    plt.grid(True)
+
+    plt.show()
