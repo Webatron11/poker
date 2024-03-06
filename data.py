7abc6ffddab8186968ad129d7fa1a8a72e2d6cc4
Oscar <oscarwebb162@protonmail.com>
2024-03-06 22:11:13 +1100

    AAAAAAAH

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
+from sqlite3 import connect
 
 
 class Player:
-    def __init__(self, name: str, balance: int, buyins: float, revbuyins: int, balanceovertime: list, profitovertime: list):
+    def __init__(self, name, balance, buyins, revbuyins, balanceovertime, profitovertime):
         self.name = name
         self.balance = balance
         self.buyins = buyins
@@ -11,30 +12,37 @@ class Player:
 
 
 class Session:
-    def __init__(self, number, players, buyins, revbuyins, oscar, mitchell, xavier, ben, aidan, hunter, cooper):
+    def __init__(self, number, players, buyins, revbuyins, balances):
         self.number = number
         self.players = players
         self.buyins = buyins
         self.revbuyins = revbuyins
-        self.oscar = oscar
-        self.mitchell = mitchell
-        self.xavier = xavier
-        self.ben = ben
-        self.aidan = aidan
-        self.hunter = hunter
-        self.cooper = cooper
+        self.balances = balances
 
+
+conn = connect('poker.db')
+cur = conn.cursor()
+
+columns = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('poker')")
+
+print(columns)
+
+players = []
+for i in columns:
+    players.append(Player(i, 0, 0, 0, [], []))
+
+print(players)
 
 # Player initialisation.
 
-aidan = Player("Aidan", 0, 0, 0, [], [])
-ben = Player("Ben", 0, 0, 0, [], [])
-cooper = Player("Cooper", 0, 0, 0, [], [])
-hunter = Player("Hunter", 0, 0, 0, [], [])
-mitchell = Player("Mitchell", 0, 0, 0, [], [])
-oscar = Player("Oscar", 0, 0, 0, [], [])
-xavier = Player("Xavier", 0, 0, 0, [], [])
-
-# Array of players in order to index through all players.
-
-players = [aidan, ben, cooper, hunter, mitchell, oscar, xavier]
+# aidan = Player("Aidan", 0, 0, 0, [], [])
+# ben = Player("Ben", 0, 0, 0, [], [])
+# cooper = Player("Cooper", 0, 0, 0, [], [])
+# hunter = Player("Hunter", 0, 0, 0, [], [])
+# mitchell = Player("Mitchell", 0, 0, 0, [], [])
+# oscar = Player("Oscar", 0, 0, 0, [], [])
+# xavier = Player("Xavier", 0, 0, 0, [], [])
+#
+# # Array of players in order to index through all players.
+#
+# players = [aidan, ben, cooper, hunter, mitchell, oscar, xavier]
