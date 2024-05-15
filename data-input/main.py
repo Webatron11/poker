63fa56a1947d90db665c28e45e24f466382d94c7
Oscar Webb <oscarwebb162@protonmail.com>
2024-05-15 14:30:07 +1000

    Start work on individual editing

--- data-input/main.py
+++ data-input/main.py
@@ -11,14 +11,8 @@ from functions import *
 #   Error check for correct # W # R # B # G format (regex)
 #   Recurse through the list of players inputted.
 
-today = datetime.now()
-conn = sqlite3.connect('database.db')
-cur = conn.cursor()
 
-
-def all_players(date, dbconn, dbcur):
-    session = Session(0, [], [], [], balances)
-
+def all_players(session):
     for player in players:
         if yninput(session, "players", player) == "Y":
             yninput(session, "buyins", player)
@@ -29,36 +23,58 @@ def all_players(date, dbconn, dbcur):
     checkbuy(session)
     checkrev(session)
 
-    playerbalances = "'"
-    for player in players:
-        if session.balances[player.name] is not None:
-            playerbalances += session.balances[player.name] + "', '"
 
-    fields = ', '.join([player.lower() for player in session.players]) + ", date, players, buyins, revbuyins"
-
-    values = (playerbalances +
-              date.strftime("%Y/%m/%d") +
-              "', '" + ", ".join(session.players) +
-              "', '" + ", ".join(session.buyins) +
-              "', '" + ", ".join(session.revbuyins) +
-              "'"
-              )
-
-    dbcur.execute(f"INSERT INTO poker({fields}) VALUES ({values})")
-    dbconn.commit()
-
-
-def one_player(date, dbconn, dbcur):
+def one_player(session):
     for player in players:
         print(f"{players.index(player)}. {player.name}")
-    player = input("Which player would you like to edit session data for? ")
-    if player is int:
-        player = players[player]
-    if player.lower() in players:
-        player = player.lower()
+    while True:
+        player = input("Which player would you like to edit session data for? ")
+        try:
+            int(player)
+        except ValueError:
+            if player.lower() in [i.name.lower() for i in players]:
+                session.append(player.capitalize())
+                break
+            else:
+                print("There is no player with that name. Please try again!")
+        except None:
+            if int(player) < (len(players)-1):
+                session.append(players[player].name.capitalize())
+                break
+            else:
+                print("There is no player with that ID. Please try again!")
 
 
-one_player(today, conn, cur)
+today = datetime.now()
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+session = Session(0, [], [], [], balances)
+
+edit = input("Do you want to edit one player (Y/N)").upper()
+match edit:
+    case "Y":
+        one_player(session)
+    case "N":
+        all_players(session)
+
+playerbalances = "'"
+for player in players:
+    if session.balances[player.name] is not None:
+        playerbalances += session.balances[player.name] + "', '"
+
+fields = ', '.join([player.lower() for player in session.players]) + ", date, players, buyins, revbuyins"
+
+values = (playerbalances +
+          today.strftime("%Y/%m/%d") +
+          "', '" + ", ".join(session.players) +
+          "', '" + ", ".join(session.buyins) +
+          "', '" + ", ".join(session.revbuyins) +
+          "'"
+          )
+
+cur.execute(f"INSERT INTO poker({fields}) VALUES ({values})")
+conn.commit()
 
 cur.close()
 conn.close()
