13a33ffd67e087550d0423c5b386c2d2925e0578
Oscar Webb <oscarwebb162@protonmail.com>
2024-05-15 13:00:26 +1000

    Started work

--- data-input/main.py
+++ data-input/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import sqlite3
-from data import *
+from data import Session, balances, players
 from functions import *
 
 # Ask user for players
@@ -11,40 +11,54 @@ from functions import *
 #   Error check for correct # W # R # B # G format (regex)
 #   Recurse through the list of players inputted.
 
-session = Session(0, [], [], [], balances)
-
-for i in players:
-    if yninput(session, "players", i) == "Y":
-        yninput(session, "buyins", i)
-        yninput(session, "revbuyins", i)
-
-for i in session.players:
-    balanceinput(session, i)
-
-checkbuy(session)
-checkrev(session)
-
-playerbalances = "'"
-for i in players:
-    if session.balances[i.name] is not None:
-        playerbalances += session.balances[i.name] + "', '"
-
 today = datetime.now()
-fields = ', '.join([i.lower() for i in session.players]) + ", date, players, buyins, revbuyins"
-
-values = (playerbalances +
-          today.strftime("%Y/%m/%d") +
-          "', '" + ", ".join(session.players) +
-          "', '" + ", ".join(session.buyins) +
-          "', '" + ", ".join(session.revbuyins) +
-          "'"
-          )
-
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
-cur.execute(f"INSERT INTO poker({fields}) VALUES ({values})")
-conn.commit()
+
+def all_players(date, dbconn, dbcur):
+    session = Session(0, [], [], [], balances)
+
+    for player in players:
+        if yninput(session, "players", player) == "Y":
+            yninput(session, "buyins", player)
+            yninput(session, "revbuyins", player)
+
+    for player in session.players:
+        balanceinput(session, player)
+    checkbuy(session)
+    checkrev(session)
+
+    playerbalances = "'"
+    for player in players:
+        if session.balances[player.name] is not None:
+            playerbalances += session.balances[player.name] + "', '"
+
+    fields = ', '.join([player.lower() for player in session.players]) + ", date, players, buyins, revbuyins"
+
+    values = (playerbalances +
+              date.strftime("%Y/%m/%d") +
+              "', '" + ", ".join(session.players) +
+              "', '" + ", ".join(session.buyins) +
+              "', '" + ", ".join(session.revbuyins) +
+              "'"
+              )
+
+    dbcur.execute(f"INSERT INTO poker({fields}) VALUES ({values})")
+    dbconn.commit()
+
+
+def one_player(date, dbconn, dbcur):
+    for player in players:
+        print(f"{players.index(player)}. {player.name}")
+    player = input("Which player would you like to edit session data for? ")
+    if player is int:
+        player = players[player]
+    if player.lower() in players:
+        player = player.lower()
+
+
+one_player(today, conn, cur)
 
 cur.close()
 conn.close()
