378ee70fbc7fd3677b689642570b289f71c7c169
Xavier-Waters <Xavier.Waters@protonmail.com>
2024-05-16 17:49:14 +1000

    removed add_user integrated into functions

--- scripts/add-user.py
+++
@@ -1,9 +0,0 @@
-from sqlite3 import connect
-
-conn = connect('database.db')
-cur = conn.cursor()
-
-playername = input("What is the new player's name? ").lower()
-
-cur.execute('alter table poker add %s;' % playername)
-conn.commit()
